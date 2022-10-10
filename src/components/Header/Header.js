e4a996c7389e070d274864e410bb75c3eda29f81
shakil <shakil.hossain450s@gmail.com>
2022-10-10 21:21:26 +0600

    navbar added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Bars3Icon } from '@heroicons/react/24/solid';
+export const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    return (
+        <div class="bg-white shadow-lg">
+            <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+                <div class="relative flex items-center justify-between">
+                    <Link
+                        href="/"
+                        aria-label="Company"
+                        title="Company"
+                        class="inline-flex items-center"
+                    >
+                        <span class="ml-2 text-xl font-bold tracking-wide text-black-100 uppercase">
+                            Quiz Hunter
+                        </span>
+                    </Link>
+                    <ul class="flex items-center hidden space-x-8 lg:flex">
+                        <li>
+                            <Link
+                                href="/"
+                                aria-label="Our product"
+                                title="Our product"
+                                class="font-medium tracking-wide text-black-100 transition-colors duration-200 hover:text-teal-accent-400"
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/"
+                                aria-label="Our product"
+                                title="Our product"
+                                class="font-medium tracking-wide text-black-100 transition-colors duration-200 hover:text-teal-accent-400"
+                            >
+                                Statics
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/"
+                                aria-label="Our product"
+                                title="Our product"
+                                class="font-medium tracking-wide text-black-100 transition-colors duration-200 hover:text-teal-accent-400"
+                            >
+                                Blog
+                            </Link>
+                        </li>
+                    </ul>
+                    <div class="lg:hidden">
+                        <button
+                            aria-label="Open Menu"
+                            title="Open Menu"
+                            class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+                            onClick={() => setIsMenuOpen(true)}
+                        >
+                            <Bars3Icon className="h-8 w-8 text-gray-600 font-semibold"></Bars3Icon>
+
+                        </button>
+                        {isMenuOpen && (
+                            <div class="absolute top-0 left-0 w-full">
+                                <div class="p-5 bg-white border rounded shadow-sm">
+                                    <div class="flex items-center justify-between mb-4">
+                                        <div>
+                                            <Link
+                                                href="/"
+                                                aria-label="Company"
+                                                title="Company"
+                                                class="inline-flex items-center"
+                                            >
+                                                <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                                                    Company
+                                                </span>
+                                            </Link>
+                                        </div>
+                                        <div>
+                                            <button
+                                                aria-label="Close Menu"
+                                                title="Close Menu"
+                                                class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                                                onClick={() => setIsMenuOpen(false)}
+                                            >
+                                                <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                                                    <path
+                                                        fill="currentColor"
+                                                        d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <nav>
+                                        <ul class="space-y-4">
+                                            <li>
+                                                <Link
+                                                    href="/"
+                                                    aria-label="Our product"
+                                                    title="Our product"
+                                                    class="font-medium tracking-wide text-black-100 transition-colors duration-200 hover:text-teal-accent-400"
+                                                >
+                                                    Home
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link
+                                                    href="/"
+                                                    aria-label="Our product"
+                                                    title="Our product"
+                                                    class="font-medium tracking-wide text-black-100 transition-colors duration-200 hover:text-teal-accent-400"
+                                                >
+                                                    Statics
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link
+                                                    href="/"
+                                                    aria-label="Our product"
+                                                    title="Our product"
+                                                    class="font-medium tracking-wide text-black-100 transition-colors duration-200 hover:text-teal-accent-400"
+                                                >
+                                                    Blog
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
