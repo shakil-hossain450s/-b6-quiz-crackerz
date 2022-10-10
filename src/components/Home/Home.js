2e4117f4b6e5e20104d90af43e4afe7655da8481
shakil <shakil.hossain450s@gmail.com>
2022-10-10 21:42:58 +0600

    header content added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='mt-5'>
+            <h2 className='text-4xl font-semibold'>Choose Your Quiz</h2>
+            <p className='mt-4 text-[18px]'>
+                Now you can develope your self by answering this quiz on Our Quiz Hunter Site. <br />
+                You can develope your skill and thinking.
+            </p>
+        </div>
+    );
+};
+
+export default Home;
--- src/components/router.js
+++ src/components/router.js
@@ -1,6 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
+import Home from "./Home/Home";
 
 export const router = createBrowserRouter([
-    { path: '/', element: <MainLayout></MainLayout> }
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        children: [
+            { path: '/', element: <Home></Home> },
+            { path: '/home', element: <Home></Home> }
+        ]
+    }
 ]);
