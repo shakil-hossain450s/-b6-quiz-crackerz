e51e00049a3cafef7e746c2c765b700c6ee5bec4
shakil <shakil.hossain450s@gmail.com>
2022-10-12 12:51:30 +0600

    phone responsive

--- src/components/TopicDetail/TopicDetail.js
+++ src/components/TopicDetail/TopicDetail.js
@@ -9,7 +9,7 @@ const TopicDetail = () => {
     const [names] = useContext(NameContext);
 
     return (
-        <div className='grid grid-cols-1 w-4/6 mx-auto gap-12 my-8'>
+        <div className='grid grid-cols-1 lg:w-4/6 w-[90%] mx-auto gap-12 my-8'>
             <h2 className='font-semibold text-3xl'>Question Of: {names}</h2>
             {
                 questions.map(example => <Question
